a4ea7a74388ad07753181105a6c1764c7069553c
NecronTrooper <akezhan.kumarov@gmail.com>
2025-12-11 22:53:17 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CurrencyCalculator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Select option from the list");
+         int menu = int.Parse(Console.ReadLine());
+         while (menu != 0) {
+            
+         }
+      }
+   }
+}
